6b63f7faab5e4ae56350a5b671a82c3731c02deb
Nikolay Zamotaev <nzamotaev@luxoft.com>
2017-09-06 20:03:08 +0300

    Added better visualisation

--- arduino/sonar_servo_test/sonar_servo_test.ino
+++ arduino/sonar_servo_test/sonar_servo_test.ino
@@ -1,9 +1,11 @@
 #include <Servo.h>
 #include <Ultrasonic.h>
+#include <BasicTerm.h>
 
 Servo sonar_sweeper;
 Ultrasonic usonic(5,7);
 Ultrasonic usonic_r(6,7);
+BasicTerm term(&Serial);
 
 int angle = 0;
 int direction = 7;
@@ -12,10 +14,44 @@ const int limit = 45;
 void setup() {
   pinMode(9,OUTPUT);
   digitalWrite(9,HIGH);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  term.init();
+  term.set_attribute(BT_NORMAL);
+  term.set_color(BT_WHITE,BT_BLACK);
+  term.cls();
+  term.show_cursor(true);
   sonar_sweeper.attach(2);
   sonar_sweeper.write(angle);
-  Serial.println("angle\tdistance");
+  term.position(0,0);
+  term.println(F("distance"));
+  for(int i=1;i<15;i++) {
+      term.position(i+1,4);
+      term.print('|');
+  };
+  for(int i=1;i<52;i++) {
+      term.position(1,i+4);
+      term.print('-');
+      term.position(16,i+4);
+      term.print('-');
+  };
+  term.position(1,0);
+  term.print(-50);
+  term.position(8,0);
+  term.print(0);
+  term.position(16,0);
+  term.print(50);
+};
+
+void fillgraph(int ang,long dist) {
+    int x = map(ang,-50,50,2,15);
+    int y = dist % 54;
+    term.position(x,5);
+    term.print(F("              "));
+    term.print(F("              "));
+    term.print(F("              "));
+    term.print(F("              "));
+    term.position(x,y+5);
+    term.print('*');
 };
 
 void loop() {
@@ -23,9 +59,20 @@ void loop() {
   sonar_sweeper.write(angle+90);
   delay(35);
   digitalWrite(9,HIGH);  
-  Serial.print(angle);
-  Serial.print('\t');
-  Serial.println(usonic.Ranging(CM));
+  long distance = usonic.Ranging(CM);
+  fillgraph(angle,distance);
+  term.position(20,0);
+  term.print(F("Millis:       "));
+  term.position(20,8);
+  term.print(millis());
+  term.position(21,0);
+  term.print(F("Angle :       "));
+  term.position(21,8);
+  term.print(angle);
+  term.position(22,0);
+  term.print(F("Distnc:       "));
+  term.position(22,8);
+  term.print(distance);
   angle += direction;
   if(abs(angle) > limit ) {
       direction= -direction;
--- arduino/sonar_test/sonar_test.ino
+++ arduino/sonar_test/sonar_test.ino
@@ -1,19 +1,45 @@
 #include <Ultrasonic.h>
+#include <BasicTerm.h>
+
 
 Ultrasonic usonic1(5,7);
 Ultrasonic usonic2(6,7);
 
+BasicTerm term(&Serial);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  term.init();
+  term.set_attribute(BT_NORMAL);
+  term.set_color(BT_WHITE,BT_BLACK);
+  term.cls();
+  term.show_cursor(false);
 }
 
 void loop() {
+  long range1 = usonic1.Ranging(CM);
+  long range2 = usonic2.Ranging(CM);
   // put your main code here, to run repeatedly:
-  Serial.print(usonic1.Ranging(CM));
-  Serial.print('\t');
-  delay(10);
-  Serial.print(usonic2.Ranging(CM));
-  Serial.println();
-  delay(10);
+  term.set_color(BT_RED,BT_BLACK);
+  term.position(0,0);
+  term.print(F("Front : "));
+  term.print(F("      "));
+  term.position(0,8);
+  term.print(range1);
+  delay(30);
+  term.set_color(BT_BLUE,BT_BLACK);
+  term.position(1,0);
+  term.print(F("Back  : "));
+  term.print(F("      "));
+  term.position(1,8);
+  term.print(range2);
+  delay(30);
+  term.set_color(BT_YELLOW,BT_BLACK);
+  term.position(2,0);
+  term.print(F("Millis: "));
+  term.print(F("      "));
+  term.position(2,8);
+  term.print(millis());
+  delay(30);
 }
